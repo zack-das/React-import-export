865b3983f3478eed0a477e7f7fbcb6433948d991
Allan Ochieng <alnothigo@gmail.com>
2024-11-19 08:18:37 +0300

    Initial files upload
    New files

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>
+        {username} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
